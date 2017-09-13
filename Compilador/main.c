c0d8cb7e30499f790212f8aeb5a716d3d1b8becb
natoszme <natoszmedra@gmail.com>
2017-09-13 10:35:00 -0300

    Segunda version del parser
    Corregimos la matriz, se agregaron y corrigieron las funciones que reconocen cada caracter
    Ahora levanta archivo de flujo de entrada desde la linea de comando
    Se empezo a recorrer el archivo

--- Compilador/main.c
+++ Compilador/main.c
@@ -1,9 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cambiarEstado(int **matrizLexicografica, int *estadoActual, int columnaLeida)
+int convertirCaracterANumero(char caracter)
 {
-    *estadoActual = matrizLexicografica[estadoActual][columnaLeida];
+    int valorDecimal = caracter - '0';
+    return valorDecimal;
+}
+int esNumero(char caracter)
+{
+    int valorDecimal = convertirCaracterANumero(caracter);
+    if (valorDecimal >= 0 && valorDecimal <= 9)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esMas(char caracter)
+{
+    if (caracter == '+')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esMenos(char caracter)
+{
+    if (caracter == '-')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esLetra(char caracter)
+{
+    if ((caracter >= 'a' && caracter <= 'z') || (caracter >= 'A' && caracter <= 'Z'))
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int esDosPuntos(char caracter)
+{
+    if (caracter == ':')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esIgual(char caracter)
+{
+    if (caracter == '=')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esParentesisQueAbre(char caracter)
+{
+    if (caracter == '(')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esParentesisQueCierra(char caracter)
+{
+    if (caracter == ')')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esComa(char caracter)
+{
+    if (caracter == ',')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esPuntoyComa(char caracter)
+{
+    if (caracter == ';')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esEspacio(char caracter)
+{
+    if (caracter == ' ')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int esFinDeTexto(char caracter)
+{
+    if (caracter == EOF)
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 int columnaMatrizPorCaracter(char caracterLeido)
@@ -18,53 +129,125 @@ int columnaMatrizPorCaracter(char caracterLeido)
         return 1;
     }
 
-    if (esOperador(caracterLeido))
+    if (esMas(caracterLeido))
     {
         return 2;
     }
 
-    if (esDosPuntos(caracterLeido))
+    if (esMenos(caracterLeido))
     {
         return 3;
     }
 
-    if (esIgual(caracterLeido))
+    if (esParentesisQueAbre(caracterLeido))
     {
         return 4;
     }
 
-    if (esSignoPuntuacion(caracterLeido))
+    if (esParentesisQueCierra(caracterLeido))
     {
         return 5;
     }
 
-    if (esFinDeTexto(caracterLeido))
+    if (esComa(caracterLeido))
     {
         return 6;
     }
 
+    if (esPuntoyComa(caracterLeido))
+    {
+        return 7;
+    }
+
+    if (esDosPuntos(caracterLeido))
+    {
+        return 8;
+    }
+
+    if (esIgual(caracterLeido))
+    {
+        return 9;
+    }
+
+    if (esFinDeTexto(caracterLeido))
+    {
+        return 10;
+    }
+
+    if (esEspacio(caracterLeido))
+    {
+        return 11;
+    }
+
     //7 es el estado de rechazo
-    return 7;
+    return 12;
 }
 
-int main()
+void erroresArchivo(int numeroError)
 {
-    int matrizLexicografica[11][8] = {
-        {1,3,5,6,10,8,9,10},
-        {1,1,2,2,2,2,2,10},
-        {10,10,10,10,10,10,10,10},
-        {10,3,4,4,4,4,4,10},
-        {10,10,10,10,10,10,10,10},
-        {10,10,10,10,10,10,10,10},
-        {10,10,10,10,7,10,10,10},
-        {10,10,10,10,10,10,10,10},
-        {10,10,10,10,10,10,10,10},
-        {10,10,10,10,10,10,10,10},
-        {10,10,10,10,10,10,10,10}
+	switch(numeroError)
+	{
+		case 0:
+            printf("Debe ingresar un argumento");
+		break;
+
+		case 1:
+			printf("Al menos un archivo no pudo ser abierto");
+        break;
+	}
+}
+
+
+int cambiarEstado(int **matrizLexicografica, int estadoActual, int columnaLeida)
+{
+    return matrizLexicografica[estadoActual][columnaLeida];
+}
+
+int main(int argc, char *argv[])
+{
+    int matrizLexicografica[15][13] = {
+       //L  D  +  -  (  )  ,   ; :  = fdt sp otro
+        {1, 3, 5, 6, 7, 8, 9, 10,11,14,13,0, 14},
+        {1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,12,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14},
+        {14,14,14,14,14,14,14,14,14,14,14,14,14}
     };
 
-    int estado;
+    int estado = 0;
+    char *rutaDelFlujo = argv[1];
+    FILE* flujoEntrada;
 
+    if(argc=!2)
+    {
+		errores(0);
+		return 0;
+	}
+
+	if( flujoEntrada = fopen(rutaDelFlujo, "r") )
+    {
+        char caracterLeido;
+        int columnaCaracterLeido = 0;
+
+        //chequear qué pasa con el fin de archivo
+        while( caracterLeido = getc(flujoEntrada) )
+        {
+            columnaCaracterLeido = columnaMatrizPorCaracter(caracterLeido);
+            estado = cambiarEstado(matrizLexicografica, estado, columnaCaracterLeido);
+
+
+        }
+    }
 
 
     return 0;
